2c301074cd1efa67aed0b116916a94926f666d62
hirbodprime <hirbodprime@gmail.com>
2024-04-08 13:56:17 +0330

    added user

--- comment/api/serializers.py
+++ comment/api/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from comment.models import Comment, Quote
-
+from account.api.serializers import UserProfileSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
     class Meta:
         model = Comment
-        fields = ['id', 'text', 'class_ref', 'date_added']
+        fields = ['id', 'text', 'class_ref', 'date_added','user']
         read_only_fields = ['user', 'class_ref']  # Make class_ref read-only
-
+    def get_user(self, obj):
+        # Assuming your Comment model has a field named 'user' that links to the CustomUser model
+        user = obj.user
+        return UserProfileSerializer(user).data
 class QuoteSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Quote
-        fields = ['id', 'text', 'user', 'class_ref', 'date_added']
+        fields = ['id', 'text', 'user', 'class_ref', 'date_added','user']
         read_only_fields = ['user', 'class_ref']  # Make class_ref read-only
+    def get_user(self, obj):
+        # Assuming your Comment model has a field named 'user' that links to the CustomUser model
+        user = obj.user
+        return UserProfileSerializer(user).data
