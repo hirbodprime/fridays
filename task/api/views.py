c8d61d645db1168fd1cf4246e27904a3f3d6bdd2
hirbodprime <hirbodprime@gmail.com>
2024-10-04 21:45:07 +0330

    asdf

--- task/api/views.py
+++ task/api/views.py
@@ -36,24 +36,37 @@ class TaskViewSet(viewsets.ModelViewSet):
             return Response({"error": "Authentication required."}, status=status.HTTP_401_UNAUTHORIZED)
         if not user.premium:
             return Response({"error": "Only premium users can create tasks."}, status=status.HTTP_403_FORBIDDEN)
-        
+
         serializer = self.get_serializer(data=request.data, context={'request': request})
+        
         if serializer.is_valid():
             task = serializer.save(created_by=user)
+            
             # If 'assign_all' is in request, assign all users to the task
             if request.data.get('assign_all') == True:
                 task.assigned_users.set(CustomUser.objects.all())
             task.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            # Return success: true and data in a 'data' key
+            return Response({
+                'success': True,
+                'data': serializer.data  # Wrap serialized data in 'data' key
+            }, status=status.HTTP_201_CREATED)
+        
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            # Return validation errors
+            return Response({
+                'success': False,
+                'errors': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
 
     def get_queryset(self):
         user = self.request.user
         if user.is_anonymous:
             return Task.objects.none()  # No tasks for anonymous users
         # Return tasks where the user is assigned or is the creator
-        return Task.objects.filter(assigned_users=user) | Task.objects.filter(created_by=user)
+        return Task.objects.filter(assigned_users=user)
 
     def partial_update(self, request, *args, **kwargs):
         task = self.get_object()
